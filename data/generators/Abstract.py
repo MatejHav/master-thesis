47e000b125fa442b0bc2dbb97d9c666c5c02d3ab
Matej Havelka <M.Havelka@student.tudelft.nl>
2023-11-30 12:37:41 +0100

    Add transition through states

--- data/generators/Abstract.py
+++ data/generators/Abstract.py
@@ -1,4 +1,5 @@
 from typing import *
+import numpy as np
 
 class Value:
     def __init__(self):
--- data/generators/MDP.py
+++ data/generators/MDP.py
@@ -1,21 +1,24 @@
 from collections import defaultdict
 from data.generators.Abstract import *
 
+
 class State:
     def __init__(self, X: List[Value], reward: float):
         self.X = X
         self.R = reward
 
+
 class Action:
     def __init__(self, a: List[Value]):
         self.a = a
+
 
 class MDP:
 
     def __init__(self, name: str):
         self.name = name
         self.S: Set[State] = set()
-        self.T: Dict[State, Dict[Action, Tuple[State, float]]] = dict()
+        self.T: Dict[State, Dict[Action, Dict[State, float]]] = dict()
 
     def add_state(self, state: State) -> bool:
         if state in self.S:
@@ -23,19 +26,26 @@ class MDP:
         self.S.add(state)
         return True
 
-    def add_transition(self, from_state: State, action: Action, to_state: State, p):
+    def add_transition(self, from_state: State, action: Action, to_state: State, p: float):
         if from_state not in self.T:
             self.T[from_state] = dict()
-        self.T[from_state][action] = (to_state, p)
+        if action not in self.T[from_state]:
+            self.T[from_state][action] = dict()
+        self.T[from_state][action][to_state] = p
+        assert sum(self.T[from_state][action].items()) <= 1, f"Sum of probabilities of T({from_state}, {action}) is greater than 1."
 
-    def get_actions(self, state: State) -> Dict[Action, Tuple[State, float]]:
+    def get_actions(self, state: State) -> Dict[Action, Dict[State, float]]:
         if state not in self.T:
             return dict()
         return self.T[state]
 
     def perform_action(self, state: State, action: Action) -> State:
-        pass
+        if state not in self.T:
+            raise RuntimeError(f"State {state} is a Terminal state.")
+        if action not in self.T[state]:
+            raise RuntimeError(f"Action {action} not possible from state {state}.")
+        possible_outcomes = self.T[state][action]
+        return np.random.choice(np.array(possible_outcomes.keys()), p=np.array(possible_outcomes.values()))
 
     def perform_actions(self, state: State, actions: List[Action]) -> State:
         pass
-
