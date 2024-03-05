e75978e6bd7ec0324ec653ca32d5849ef03e40eb
Matej Havelka <havelkamatej@gmail.com>
2024-03-05 21:49:05 +0100

    Add uncertainty from data

--- models/UncertaintySensitivityModels.py
+++ models/UncertaintySensitivityModels.py
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 
-def create_msm_model(data, gamma, treatement, is_lower_bound):
+def create_msm_model(data, gamma, treatement, is_lower_bound, alpha_prop, alpha_prob):
     # Data according to treated
     all_data = data
     data = data[data["T0"] == treatement]
@@ -38,7 +38,6 @@ def create_msm_model(data, gamma, treatement, is_lower_bound):
     x_and_y_features.append("Y0")
     y_probabilities = data.groupby(x_and_y_features, as_index=False).size()
     y_probabilities["probability"] = y_probabilities["size"] / sum(y_probabilities["size"])
-    n = sum(y_probabilities["size"])
 
     def propensity(x_index):
         x = X.iloc[x_index]
@@ -68,45 +67,81 @@ def create_msm_model(data, gamma, treatement, is_lower_bound):
     def get_value(y):
         return Y.iloc[y]["Y0"]
 
+    def epsilon_propensity(x_index):
+        x = X.iloc[x_index]
+        return np.sqrt(1 / x["size"] * np.log(2 / alpha_prop))
+
+    def epsilon_probability(x_index, y_index):
+        count = size(y_index, x_index)
+        if count == 0:
+            return 0
+        return np.sqrt(1 / count * np.log(2 * y_size / alpha_prob))
+
     model = ConcreteModel(name="MarginalSensitivityModel")
     model.X = RangeSet(0, x_size - 1)
     model.Y = RangeSet(0, y_size - 1)
     model.lam = Var(model.X, model.Y, bounds=(1 / gamma, gamma), initialize=1)
+    model.propensity = Var(model.X, bounds=(0, 1))
+    model.prob = Var(model.X, model.Y, bounds=(0, 1))
 
     # Constraint 1: Lambda is a distribution of Y | X, T
     def distribution_constraint(model):
-        return sum([sum(model.lam[x, y] * size(y ,x) for y in model.Y) for x in model.X]) / n == 1
+        return sum([sum(model.lam[x, y] * model.prob[x, y] for y in model.Y) * X.iloc[x]["size"] for x in model.X]) / sum(X["size"]) == 1
 
     model.c1 = Constraint(rule=distribution_constraint)
 
     # Constraint 2: Propensity scores remains unchanged
     def propensity_constraint(model):
-        return sum([sum([propensity(x) * model.lam[x, y] * size(y, x) for y in model.Y]) for x in model.X]) / n == sum(
-            [sum([propensity(x) * size(y, x) for y in model.Y]) for x in model.X]) / n
+        return sum([sum([model.propensity[x] * model.lam[x, y] * model.prob[x, y] for y in model.Y]) for x in model.X]) == sum(
+            [sum([model.propensity[x] * model.prob[x, y] for y in model.Y]) for x in model.X])
 
     model.c2 = Constraint(rule=propensity_constraint)
 
     # Constraint 3: Distribution of Y unchanged
     def p_constraint(model):
-        return sum([sum([p(y, x) * model.lam[x, y] * X.iloc[x]["size"] for x in model.X]) / sum(X["size"]) for y in model.Y]) == sum([sum(
-            [p(y, x) * X.iloc[x]["size"] for x in model.X]) / sum(X["size"]) for y in model.Y])
+        return sum([sum([model.prob[x, y] * model.lam[x, y] * X.iloc[x]["size"] for x in model.X]) / sum(X["size"]) for y in
+                    model.Y]) == sum([sum(
+            [model.prob[x, y] * X.iloc[x]["size"] for x in model.X]) / sum(X["size"]) for y in model.Y])
 
     model.c3 = Constraint(rule=p_constraint)
 
+    # Constraint 4: Propensity definition
+    def propensity_definition_constraint(model, x):
+        expected_propensity = propensity(x)
+        bound = epsilon_propensity(x)
+        return max(0, expected_propensity - bound), model.propensity[x], min(1, expected_propensity + bound)
+
+    model.c4 = Constraint(model.X, rule=propensity_definition_constraint)
+
+    # Constraint 5: Probability of Y given X definition
+    def probability_of_y_constraint(model, x, y):
+        expected_prob = p(y, x)
+        bound = epsilon_probability(x, y)
+        return max(0, expected_prob - bound), model.prob[x, y], min(1, expected_prob + bound)
+
+    model.c5 = Constraint(model.X, model.Y, rule=probability_of_y_constraint)
+
+    # Constraint 6: Probability over y is 1
+    def probability_distribution_constraint(model, x):
+        return sum([model.prob[x, y] for y in model.Y]) == 1
+
+    model.c6 = Constraint(model.X, rule=probability_distribution_constraint)
+
     # Objective: Find the bound
     def objective_function(model):
-        return sum([sum([get_value(y) * model.lam[x, y] * size(y, x) for y in model.Y]) for x in model.X]) / n
+        return sum([sum([get_value(y) * model.lam[x, y] * model.prob[x, y] for y in model.Y]) * X.iloc[x]["size"] for x in model.X]) / sum(X["size"])
 
     model.OBJ = Objective(rule=objective_function,
                           sense=pyomo.core.minimize if is_lower_bound else pyomo.core.maximize)
 
-    opt = SolverFactory('glpk')
+    opt = SolverFactory('ipopt')
     opt.solve(model)
     # model.display()
+    # exit()
     return model.OBJ()
 
 
-def create_f_sensitivity_model(data, rho, treatment, is_lower_bound):
+def create_f_sensitivity_model(data, rho, treatment, is_lower_bound, alpha_prop, alpha_prob):
     # Data according to treated
     all_data = data
     p_treated = len(data[data["T0"] == treatment]) / len(all_data)
@@ -153,50 +188,85 @@ def create_f_sensitivity_model(data, rho, treatment, is_lower_bound):
             return 0
         return selection["size"].iloc[0]
 
+    def p(y_index, x_index):
+        x = X.iloc[x_index]
+        y = Y.iloc[y_index]
+        selection = y_probabilities[
+            np.all(y_probabilities[x_features] == x[x_features], axis=1) & np.all(y_probabilities[["Y0"]] == y[["Y0"]],
+                                                                                  axis=1)]
+        if len(selection) == 0:
+            return 0
+        return selection["size"].iloc[0] / x["size"]
+
     def get_value(y):
         return Y.iloc[y]["Y0"]
 
-    def r(x):
-        return (1 - propensity(x)) * p_treated / (propensity(x) * (1 - p_treated))
+    def epsilon_propensity(x_index):
+        x = X.iloc[x_index]
+        return np.sqrt(1 / x["size"] * np.log(2 / alpha_prop))
+
+    def epsilon_probability(x_index, y_index):
+        count = size(y_index, x_index)
+        if count == 0:
+            return 0
+        return np.sqrt(1 / count * np.log(2 * y_size / alpha_prob))
+
 
     model = ConcreteModel(name="FSensitivityModel")
     model.X = RangeSet(0, x_size - 1)
     model.Y = RangeSet(0, y_size - 1)
     model.L = Var(model.X, model.Y, initialize=1, within=NonNegativeReals)
+    model.propensity = Var(model.X, bounds=(0, 1))
+    model.prob = Var(model.X, model.Y, bounds=(0, 1))
+
+    def r(model, x):
+        mean = sum([model.propensity[x] * X.iloc[x]["size"] for x in model.X]) / sum(X["size"])
+        return (1 - model.propensity[x]) * mean / (model.propensity[x] * (1 - mean) + 0.00001)
 
     # Constraint 1: Definition of R
     def r_constraint(model, x):
-        return r(x) == sum([model.L[x, y] * size(y, x) for y in model.Y]) / X.iloc[x]["size"]
+        return r(model, x) == sum([model.L[x, y] * model.prob[x, y] for y in model.Y])
 
     model.c1 = Constraint(model.X, rule=r_constraint)
 
-    # # Constraint 2: R is a distribution
-    # def r_distribution_constraint(model):
-    #     return sum([model.R[x] for x in model.X]) == 1
-    #
-    # model.c2 = Constraint(rule=r_distribution_constraint)
-
-    # Constraint 3: MSM assumption
+    # Constraint 2: MSM assumption
     def f_constraint(model, x):
-        return sum([f(model.L[x, y] / r(x)) * size(y, x) for y in model.Y]) / X.iloc[x]["size"] <= rho
+        return sum([f(model.L[x, y] / r(model, x)) * model.prob[x, y] for y in model.Y]) <= rho
 
-    model.c3 = Constraint(model.X, rule=f_constraint)
+    model.c2 = Constraint(model.X, rule=f_constraint)
 
-    # # Constraint 4: L distribution constraint
-    # def l_distribution_constraint(model):
-    #     return sum([sum([model.L[x, y] for y in model.Y]) for x in model.X]) / (x_size * y_size) == 1
-    #
-    # model.c4 = Constraint(rule=l_distribution_constraint)
+    # Constraint 3: Propensity definition
+    def propensity_definition_constraint(model, x):
+        expected_propensity = propensity(x)
+        bound = epsilon_propensity(x)
+        return max(0, expected_propensity - bound), model.propensity[x], min(1, expected_propensity + bound)
+
+    model.c3 = Constraint(model.X, rule=propensity_definition_constraint)
+
+    # Constraint 4: Probability of Y given X definition
+    def probability_of_y_constraint(model, x, y):
+        expected_prob = p(y, x)
+        bound = epsilon_probability(x, y)
+        return max(0, expected_prob - bound), model.prob[x, y], min(1, expected_prob + bound)
+
+    model.c4 = Constraint(model.X, model.Y, rule=probability_of_y_constraint)
+
+    # Constraint 5: Probability over y is 1
+    def probability_distribution_constraint(model, x):
+        return sum([model.prob[x, y] for y in model.Y]) == 1
+
+    model.c5 = Constraint(model.X, rule=probability_distribution_constraint)
+
 
     # Objective: E[Y * L(X, Y)]
     def objective_function(model):
-        return sum([sum([get_value(y) * model.L[x, y] * size(y, x) for y in model.Y]) for x in model.X]) / n
+        return sum([sum([get_value(y) * model.L[x, y] * model.prob[x, y] for y in model.Y]) * X.iloc[x]["size"] for x in model.X]) / sum(X["size"])
 
     model.OBJ = Objective(rule=objective_function,
                           sense=pyomo.core.minimize if is_lower_bound else pyomo.core.maximize)
 
     opt = SolverFactory('ipopt')
-    opt.options['max_iter'] = 1000
+    # opt.options['max_iter'] = 1000
     # opt.options['acceptable_tol'] = 0.000001
     opt.solve(model)
     # model.display()
@@ -205,9 +275,9 @@ def create_f_sensitivity_model(data, rho, treatment, is_lower_bound):
 
 
 def bounds_creator(data, sensitivity_model, sensitivity_measure):
-    sensitivity = (lambda t, l: create_msm_model(data, gamma=sensitivity_measure, treatement=t, is_lower_bound=l)) \
+    sensitivity = (lambda t, l: create_msm_model(data, gamma=sensitivity_measure, treatement=t, is_lower_bound=l, alpha_prop=0.025, alpha_prob=0.025)) \
         if sensitivity_model == 'msm' else \
-        lambda t, l: create_f_sensitivity_model(data, rho=sensitivity_measure, treatment=t, is_lower_bound=l)
+        lambda t, l: create_f_sensitivity_model(data, rho=sensitivity_measure, treatment=t, is_lower_bound=l, alpha_prop=0.025, alpha_prob=0.025)
     lower_control_bound = sensitivity(0, True)
     lower_treated_bound = sensitivity(1, True)
     upper_control_bound = sensitivity(0, False)
@@ -222,13 +292,13 @@ def bounds_creator(data, sensitivity_model, sensitivity_measure):
 
 
 if __name__ == '__main__':
-    p = "5"
+    p = "01"
     df = pd.read_csv(f"../csv_files/regular_{p}.csv")
     lower_res = []
     upper_res = []
     control = []
     treated = []
-    gammas = np.linspace(1, 10, 10)
+    gammas = np.linspace(1, 3, 10)
     for gamma in gammas:
         y_control, y_treated, lower_control, lower_treated, upper_control, upper_treated = bounds_creator(df,
                                                                                                           sensitivity_model='msm',
@@ -266,7 +336,7 @@ if __name__ == '__main__':
     upper_res = []
     control = []
     treated = []
-    rhos = np.linspace(0, 0.3, 10)
+    rhos = np.linspace(0, 0.3, 4)
     p_treated = len(df[df["T0"] == 1]) / len(df)
     for rho in rhos:
         y_control, y_treated, lower_control, lower_treated, upper_control, upper_treated = bounds_creator(df,
